ff7d1d53f0886dc5de4e774ffee66542cc707e05
Jonas Ernst <D441851@burda.com>
2021-03-08 16:20:17 +0100

    pfusch

--- js/topscroller.js
+++ js/topscroller.js
@@ -1,13 +1,15 @@
 // ===== Scroll to Top ==== 
+var btn = $('#topscroller');
+
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
+  if ($(window).scrollTop() > 50) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '50');
 });
