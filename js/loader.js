035417ca5d763aa0981ccb9bd849f83658ae909d
Jonas Ernst <D441851@burda.com>
2021-03-14 11:41:53 +0100

    page loader

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,5 @@
 window.onload = function(){
     //hide the preloader
     document.querySelector(".fullpage-loader").style.opacity = "0";
+    document.querySelector(".fullpage-loader").style.display = "none";
 }
