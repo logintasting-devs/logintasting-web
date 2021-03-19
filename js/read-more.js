269bad3b28abd9d191290c0310b91ae4b0dbd843
Jonas Ernst <D441851@burda.com>
2021-03-19 12:39:57 +0100

    read more vorbereitet

--- js/read-more.js
+++ js/read-more.js
@@ -1,20 +1,3 @@
 document.addEventListener("DOMContentLoaded", e => {
-    $(document).ready(function(){
-        if(window.matchMedia("(max-width: 767px)").matches){    
-            $("#conent_hide").hide();
-                $("#re").click(function(){ 
-                x=$("#re").text();
-                
-                $("#conent_hide").slideToggle( function(){
-
-                        if(x == "Mehr..."){
-                        $("#re").text("weniger...").css({color:"#59A87B"});
-                        }
-                        else{
-                        $("#re").text("Mehr...").css({color:"#004D3E"});
-                        }
-                });
-            });
-        }
-    });
+    
 })
