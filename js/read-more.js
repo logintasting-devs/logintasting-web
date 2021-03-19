5bab13305a88ac5a34dad733ab7cab9a6f8d25d5
Jonas Ernst <D441851@burda.com>
2021-03-19 11:57:59 +0100

    read more

--- js/read-more.js
+++ js/read-more.js
@@ -1,14 +1,18 @@
-$("#conent_hide").hide();
-	$("#re").click(function(){ 
-	x=$("#re").text();
-	
-	$("#conent_hide").slideToggle( function(){
+$(document).ready(function(){
+    if(window.matchMedia("(max-width: 767px)").matches){    
+        $("#conent_hide").hide();
+            $("#re").click(function(){ 
+            x=$("#re").text();
+            
+            $("#conent_hide").slideToggle( function(){
 
-			if(x == "Mehr..."){
-			$("#re").text("weniger...").css({color:"#004D3E"});
-			}
-			else{
-			$("#re").text("Mehr...").css({color:"#59A87B"});
-			}
-	});
-	});
+                    if(x == "Mehr..."){
+                    $("#re").text("weniger...").css({color:"#59A87B"});
+                    }
+                    else{
+                    $("#re").text("Mehr...").css({color:"#004D3E"});
+                    }
+            });
+        });
+    }
+});
