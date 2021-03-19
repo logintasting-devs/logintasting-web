873c2020c69384174d5566d21407ef2994a380c6
Jonas Ernst <D441851@burda.com>
2021-03-19 12:22:33 +0100

    read more

--- js/read-more.js
+++ js/read-more.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
-    if(window.matchMedia("(max-width: 767px)").matches){    
-        $("#conent_hide").hide();
-            $("#re").click(function(){ 
-            x=$("#re").text();
-            
-            $("#conent_hide").slideToggle( function(){
+document.addEventListener("DOMContentLoaded", e => {
+    $(document).ready(function(){
+        if(window.matchMedia("(max-width: 767px)").matches){    
+            $("#conent_hide").hide();
+                $("#re").click(function(){ 
+                x=$("#re").text();
+                
+                $("#conent_hide").slideToggle( function(){
 
-                    if(x == "Mehr..."){
-                    $("#re").text("weniger...").css({color:"#59A87B"});
-                    }
-                    else{
-                    $("#re").text("Mehr...").css({color:"#004D3E"});
-                    }
+                        if(x == "Mehr..."){
+                        $("#re").text("weniger...").css({color:"#59A87B"});
+                        }
+                        else{
+                        $("#re").text("Mehr...").css({color:"#004D3E"});
+                        }
+                });
             });
-        });
-    }
-});
+        }
+    });
+})
