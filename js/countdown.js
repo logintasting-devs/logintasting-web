4d60f5bfc0d4f3cf9e643950c0e7749ee096a5c7
azubi-mr <moritz.ringwald@burda.com>
2021-03-19 14:24:46 +0100

    time format change

--- js/countdown.js
+++ js/countdown.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", e => {
     // Set the date we're counting down to
-    var countDownDate = new Date("4, 30, 2021 19:00:00").getTime();
+    var countDownDate = new Date("10/26/2020 12:29:29").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
 
     // Get today's date and time
     var now = new Date().getTime();
-        
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-        
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
+
     // Output the result in an element with id="demo"
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
