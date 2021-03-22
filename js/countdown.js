e3d6bc7b219c01169dcca82aa2b473c7f0f7efb8
Jonas Ernst <D441851@burda.com>
2021-03-22 08:47:54 +0100

    Countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,38 +1,34 @@
 document.addEventListener("DOMContentLoaded", e => {
-    // Set the date we're counting down to
-    var countDownDate = new Date("10/26/2020 12:29:29").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-     // If the count down is under 7 days, show seconds
-    if (distance < 604800000) {
-      document.querySelector(".seconds").style.display = "inline-block";
-    }
-      // If the count down is finished, show button
-    if (distance < 0) {
-        clearInterval(x);
-        document.querySelector(".date").style.display = "none";
-        document.querySelector(".livestream-button").style.display = "flex";
-    }
-
-    }, 1000);
+  (function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    let endtime = "Apr 30, 2021 19:00:00",
+        countDown = new Date(endtime).getTime(),
+        x = setInterval(function() {    
+  
+          let now = new Date().getTime(),
+              distance = countDown - now;
+  
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          // If the count down is under 7 days, show seconds
+          if (distance < 604800000) {
+            document.querySelector(".seconds").style.display = "inline-block";
+          }
+            // If the count down is finished, show button
+          if (distance < 0) {
+              clearInterval(x);
+              document.querySelector(".date").style.display = "none";
+              document.querySelector(".livestream-button").style.display = "flex";
+              clearInterval(x);
+          }
+        }, 0)
+    }());
 })
